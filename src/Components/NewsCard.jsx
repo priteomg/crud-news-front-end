11283d3ea02eb31f28f69f882b8050cf85ca4dc5
phariwud <phariwud.p@mapboss.co.th>
2021-04-26 18:54:28 +0700

    add edit del btn

--- src/Components/NewsCard.jsx
+++ src/Components/NewsCard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../css/NewsCard.css";
+import { Button, Grid } from "@material-ui/core";
 
 const NewsCard = ({
   title = "",
@@ -10,11 +11,28 @@ const NewsCard = ({
   return (
     <>
       <div className="card">
-        <div className="title">{title}</div>
-        <div className="content">{content}</div>
-        <div className="createAt">{createdAt}</div>
-
-        <div className="author">{author}</div>
+        <Grid container>
+          <Grid xs={10} item>
+            <div className="title">{title}</div>
+            <div className="content">{content}</div>
+            <div className="createAt">{createdAt}</div>
+            <div className="author">{author}</div>
+          </Grid>
+          <Grid
+            xs={2}
+            item
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+            }}
+          >
+            <div className="buttonGroup">
+              <Button>Edit</Button>
+              <Button>Delete</Button>
+            </div>
+          </Grid>
+        </Grid>
       </div>
     </>
   );
